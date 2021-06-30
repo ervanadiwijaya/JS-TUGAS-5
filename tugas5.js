77acaf21dcc0c07e585c36d5d451fee577d475d9
jauhhari <ervanadiwijaya@gmail.com>
2021-06-30 15:32:55 +0700

    OK

---
+++ tugas5.js
@@ -0,0 +1,15 @@
+
+//make function
+function newFunction() {
+
+    //make loop to take an odd value
+    for (i = 1; i <= 20; i++) {
+        if (i % 3 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+//call function
+newFunction();
+
